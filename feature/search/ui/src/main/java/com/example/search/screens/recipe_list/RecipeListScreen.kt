4bb2b655a5892302f2e53f6b1c3c40ae3e514eb4
HyungwonJin <hwjin92@gmail.com>
2025-01-11 22:33:23 +0900

    feat: list style

--- feature/search/ui/src/main/java/com/example/search/screens/recipe_list/RecipeListScreen.kt
+++ feature/search/ui/src/main/java/com/example/search/screens/recipe_list/RecipeListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Spacer
@@ -31,6 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.common.utils.UiText
@@ -49,8 +51,15 @@ fun RecipeListScreen(
 
     Scaffold(topBar = {
         TextField(
+            placeholder = {
+                Text(
+                    text = "Search here...",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            },
             value = query.value, onValueChange = {
                 query.value = it
+                viewModel.onEvent(RecipeList.Event.SearchRecipe(query.value))
             }, colors = TextFieldDefaults.colors(
                 focusedIndicatorColor = Color.Transparent,
                 unfocusedContainerColor = Color.Transparent,
@@ -81,10 +90,15 @@ fun RecipeListScreen(
 
         uiState.value.data?.let { list ->
 
-            LazyColumn(modifier = Modifier.padding(it).fillMaxSize()) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+            ) {
                 items(list) {
                     Card(
-                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
+                        modifier = Modifier
+                            .padding(horizontal = 12.dp, vertical = 4.dp)
                             .clickable { onClick(it.idMeal) },
                         shape = RoundedCornerShape(12.dp)
                     ) {
@@ -98,37 +112,55 @@ fun RecipeListScreen(
                         )
 
                         Spacer(modifier = Modifier.height(12.dp))
-
-                        Text(text = it.strMeal, style = MaterialTheme.typography.bodyLarge)
-                        Spacer(modifier = Modifier.height(12.dp))
-
-                        if (it.strTags.isNotEmpty()) {
-                            FlowRow {
-                                it.strTags.split(",")
-                                    .forEach {
-                                        Box(
-                                            modifier = Modifier.wrapContentSize().background(
-                                                color = Color.White,
-                                                shape = RoundedCornerShape(12.dp)
-                                            ).clip(RoundedCornerShape(12.dp)).border(
-                                                width = 1.dp,
-                                                color = Color.Red,
-                                                shape = RoundedCornerShape(12.dp)
-                                            ),
-                                            contentAlignment = Alignment.Center
-                                        ) {
-                                            Text(
-                                                text = it,
-                                                style = MaterialTheme.typography.bodySmall,
-                                                modifier = Modifier.padding(
-                                                    horizontal = 8.dp,
-                                                    vertical = 4.dp
-                                                )
-                                            )
-                                        }
-                                    }
-                            }
+                        Column(
+                            modifier = Modifier
+                                .padding(vertical = 12.dp)
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                        ) {
+                            Text(text = it.strMeal, style = MaterialTheme.typography.bodyLarge)
                             Spacer(modifier = Modifier.height(12.dp))
+
+                            Text(
+                                text = it.strInstructions,
+                                style = MaterialTheme.typography.bodyMedium,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 4
+                            )
+                            Spacer(modifier = Modifier.height(12.dp))
+                            if (it.strTags.isNotEmpty()) {
+                                FlowRow {
+                                    it.strTags.split(",")
+                                        .forEach {
+                                            Box(
+                                                modifier = Modifier
+                                                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                                                    .wrapContentSize()
+                                                    .background(
+                                                        color = Color.White,
+                                                        shape = RoundedCornerShape(24.dp)
+                                                    )
+                                                    .clip(RoundedCornerShape(24.dp))
+                                                    .border(
+                                                        width = 1.dp,
+                                                        color = Color.Red,
+                                                        shape = RoundedCornerShape(24.dp)
+                                                    ),
+                                                contentAlignment = Alignment.Center
+                                            ) {
+                                                Text(
+                                                    text = it,
+                                                    style = MaterialTheme.typography.bodySmall,
+                                                    modifier = Modifier.padding(
+                                                        horizontal = 12.dp,
+                                                        vertical = 6.dp
+                                                    )
+                                                )
+                                            }
+                                        }
+                                }
+                                Spacer(modifier = Modifier.height(12.dp))
+                            }
                         }
                     }
                 }
